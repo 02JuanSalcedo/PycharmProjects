bed262ff4c89600da2c37d0b5cbac67a0427440a
Juan Salcedo <02Juan.Salcedo@gulllakecs.org>
2019-05-03 12:36:15 -0400

    Added solving linear equations

--- Plot linear graphs.py
+++ Plot linear graphs.py
@@ -1,10 +1,16 @@
+'''
 # Juan Salcedo
 # Plotting linear graphs
 # April 26, 2019
 
+# Attempting to use user input instead
+# of previously defined linear equation
+
+
 # Calling necessary libraries
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 # Defining x and y
 x = np.linspace(-5, 5, 100)
@@ -27,3 +33,35 @@ plt.grid()
 
 # Calling/showing plot
 plt.show()
+'''
+
+import numpy as np
+
+# Solving linear equations
+A = np.array([[3, -9], [2, 4]])
+b = np.array([-42, 2])
+z = np.linalg.solve(A, b)
+print(z)
+
+M = np.array([[1, -2, -1], [2, 2, -1], [-1, -1, 2]])
+c = np.array([6, 1, 1])
+y = np.linalg.solve(M, c)
+print(y)
+
+# Solving non-linear equations
+
+from scipy.optimize import fsolve
+
+def myFunction(z):
+   x = z[0]
+   y = z[1]
+   w = z[2]
+
+   F = np.empty((3))
+   F[0] = x**2+y**2-20
+   F[2] = w + 5 - x*y
+   return F
+
+zGuess = np.array([1,1,1])
+z = fsolve(myFunction,zGuess)
+print(z)
